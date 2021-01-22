98d391bb5b753017f309e029d0d5c16a68be9e9f
steven.wan <951520698@qq.com>
2021-01-22 10:38:38 +0800

    basic spring-boot framework with springWeb and look ,maven,mysql drive

---
+++ src/main/java/com/stevenwan/svlas/SvlasApplication.java
@@ -0,0 +1,13 @@
+package com.stevenwan.svlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvlasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevenwan/svlas/SvlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenwan.svlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
